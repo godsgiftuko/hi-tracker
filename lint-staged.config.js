fb30ca24eb4d6282c630fcd5ebf82828ba96415d
God'sgift Uko <godsgiftuko@gmail.com>
2023-03-20 06:15:52 +0100

    Remove yarn lock

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   '*.ts': [
-    'eslint --fix',
-    'git add'
   ],
   // '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
   //   'yarn lint:prettier --parser json',
